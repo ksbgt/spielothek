5644d754106113e0397eb5ade75dc0fa6d750bd4
ksbgt <peter.hatschbach@gmail.com>
2025-10-30 20:03:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ if (btn) {
       });
 
 
-      const resLocal = await fetch("https://ksbgt.github.io/spielothek/");
+      const resLocal = await fetch("https://ksbgt.github.io/spielothek/Artikel.json");
       const daten = await resLocal.json();
 
       const vorherigeLaenge = contacts.length; // merken, ob schon Daten da waren
